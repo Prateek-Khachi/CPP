d5ebf48e0122336c66a7acc98a4775e59626940e
Prateek-Khachi <145461958+Prateek-Khachi@users.noreply.github.com>
2024-12-28 23:30:41 +0530

    CPP

--- test.cpp
+++ test.cpp
@@ -13,19 +13,25 @@ struct RetrieveKey
 int main()
 {
 
-    map<vector<int>, int> m;
-    vector<int> keys;
+//     map<vector<int>, int> m;
+//     vector<int> keys;
 
-    m[{1, 2, 3, 4}] = 2;
+//     m[{1, 2, 3, 4}] = 2;
 
-    m[{2, 4, 5, 6}] = 1;
+//     m[{2, 4, 5, 6}] = 1;
 
-    m[{1, 2, 3, 8}] = 3;
-    // Trying to update an existing key
+//     m[{1, 2, 3, 8}] = 3;
+//     // Trying to update an existing key
 
-    // Retrieve all keys
-    transform(m.begin(), m.end(), back_inserter(keys), RetrieveKey());
+//     // Retrieve all keys
+//     transform(m.begin(), m.end(), back_inserter(keys), RetrieveKey());
 
-    // Dump all keys
-    copy(keys.begin(), keys.end(), ostream_iterator<int>(cout, "\n"));
+//     // Dump all keys
+//     copy(keys.begin(), keys.end(), ostream_iterator<int>(cout, "\n"));
+// }
+
+
+    cout<<log10(128)<<endl;
+    
+    cout<<floor(pow(10,2.10))<<endl;
 }
